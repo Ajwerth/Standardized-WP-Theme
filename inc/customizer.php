a3363d8b74be436f732828105d84398447ef0141
Alec Wertheimer <alec.wertheimer@gmail.com>
2018-10-09 10:47:30 -0400

    Initial Commit 3

---
+++ inc/customizer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * 1seo_Theme Theme Customizer
+ *
+ * @package 1seo_Theme
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function one_seo_theme_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+
+	if ( isset( $wp_customize->selective_refresh ) ) {
+		$wp_customize->selective_refresh->add_partial( 'blogname', array(
+			'selector'        => '.site-title a',
+			'render_callback' => 'one_seo_theme_customize_partial_blogname',
+		) );
+		$wp_customize->selective_refresh->add_partial( 'blogdescription', array(
+			'selector'        => '.site-description',
+			'render_callback' => 'one_seo_theme_customize_partial_blogdescription',
+		) );
+	}
+}
+add_action( 'customize_register', 'one_seo_theme_customize_register' );
+
+/**
+ * Render the site title for the selective refresh partial.
+ *
+ * @return void
+ */
+function one_seo_theme_customize_partial_blogname() {
+	bloginfo( 'name' );
+}
+
+/**
+ * Render the site tagline for the selective refresh partial.
+ *
+ * @return void
+ */
+function one_seo_theme_customize_partial_blogdescription() {
+	bloginfo( 'description' );
+}
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function one_seo_theme_customize_preview_js() {
+	wp_enqueue_script( 'one_seo_theme-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'one_seo_theme_customize_preview_js' );
