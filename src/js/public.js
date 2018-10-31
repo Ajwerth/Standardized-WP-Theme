f9b08075f7689ddaefd983ad5d9c9cf9f72ea958
freddiemixell <fmixell@gmail.com>
2018-10-31 13:42:44 -0400

    Added sass for example

--- src/js/public.js
+++ src/js/public.js
@@ -2,7 +2,7 @@
 
 (function($) {
     $(function() {
-        alert('weiners');
+        alert('WEBPACK!!');
         const isBabel = !(class {}.toString().indexOf('class ') === 0);
         console.log(isBabel);
     });
